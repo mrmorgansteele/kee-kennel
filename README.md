97d2908a7dd3eb3019be889bd0a4f440889af3a5
Morgan Steele <morgan@morgansteele.info>
2025-02-15 18:22:24 -0500

    Install LightningCSS with Sass & add some initial styles

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Kee's Kennel is just a simple place to store all those pesky keyboard shortcuts I use for a week and then forget over a long weekend!
 
+
+
+
+## Components
+
+* Eleventy (11ty)
+* LightningCSS with Sass
+
---
+++ eleventy.config.js
@@ -0,0 +1,7 @@
+import eleventySass from "@11tyrocks/eleventy-plugin-sass-lightningcss";
+
+export default function (eleventyConfig) {
+  //
+  eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addWatchTarget("./_assets/css/scss/base/");
+};
